a61658892443cdb09e5c2c05f63ac99ad8da352b
Eric Boutueil <e.boutueil@gmail.com>
2019-02-05 15:18:38 +0100

    added sample side bar

--- functions.php
+++ functions.php
@@ -136,6 +136,16 @@ function ericb_template_widgets_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
+
+  register_sidebar( array(
+    'name'          => esc_html__( 'Sample-sidebar', 'ericb_template' ),
+    'id'            => 'sample-sidebar-1',
+    'description'   => esc_html__( 'This is a sample sidebar available from widget menu.', 'ericb_template' ),
+    'before_widget' => '<section id="%1$s" class="widget %2$s">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h2 class="widget-title">',
+    'after_title'   => '</h2>',
+  ) );
 }
 add_action( 'widgets_init', 'ericb_template_widgets_init' );
 
